ef521192794e64011d1a7af41d1944b4e4f19f4f
juanber84 <juanber84@gmail.com>
2016-11-10 00:03:05 +0100

    Auth ldap first version

--- src/lib/services/cmsPassport.js
+++ src/lib/services/cmsPassport.js
@@ -1,6 +1,7 @@
 import passport from 'passport';
 import LocalStrategy from 'passport-local';
 import { User } from '~/src/lib/models';
+import ldap from '~/src/lib/services/ldapService';
 
 const localStrategyOptions = {
     usernameField: 'email',
@@ -9,24 +10,58 @@ const localStrategyOptions = {
 };
 
 const localLogin = new LocalStrategy(localStrategyOptions, function(req, email, password, done) {
-    User.findOne({email: email}, function (err, user) {
-        if (err || !user) {
-            return done(null, false, req.flash('loginMessage', 'Your login details could not be verified. Please try again'));
+
+    ldap.bind(email, password)
+        .then(function (data) {
+            if (data.status && data.status == 'good') 
+                return User.findOne({ email: email })
+            else
+                dbUser(email, passport)
+        })
+        .then(function (existingUser) {
+            if (existingUser) {
+                req.flash('loginMessage', null);
+                return done(null, existingUser);
+            } else {
+
+                let user = new User({
+                    email: email,
+                    password: password,
+                    username: email.split("@")[0],
+                    tokens: []
+                });
+                return user.save()
+            }
+        })
+        .then(function(user) {
+            req.flash('loginMessage', null);
+            return done(null, user)
+        })
+
+        // Authenticate to db
+        var dbUser = function(email, passport){
+
+            User.findOne({email: email}, function (err, user) {
+                if (err || !user) {
+                    return done(null, false, req.flash('loginMessage', 'Your login details could not be verified. Please try again'));
+                }
+
+                user.comparePassword(password, function (err, isMatch) {
+                    if (err || !isMatch) {
+                        return done(null, false, req.flash('loginMessage', 'Your login details could not be verified. Please try again'));
+                    }
+
+                    if (!user.role || (user.role != 'Admin' && user.role != 'SuperAdmin')) {
+                        return done(null, false, req.flash('loginMessage', 'You do not have permissions'));
+                    }
+
+                    req.flash('loginMessage', null);
+                    return done(null, user);
+                });
+            });
+
         }
 
-        user.comparePassword(password, function (err, isMatch) {
-            if (err || !isMatch) {
-                return done(null, false, req.flash('loginMessage', 'Your login details could not be verified. Please try again'));
-            }
-
-            if (!user.role || (user.role != 'Admin' && user.role != 'SuperAdmin')) {
-                return done(null, false, req.flash('loginMessage', 'You do not have permissions'));
-            }
-
-            req.flash('loginMessage', null);
-            return done(null, user);
-        });
-    });
 });
 
 passport.use('localCMS', localLogin);
